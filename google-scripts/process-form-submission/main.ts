881cdd5b7073d71e38e11feadde60cf609167264
JohnFarrellDev <johnfarrell@fastmail.com>
2021-08-18 21:12:01 +0100

    update config to only push new appsscript on main with approval

--- google-scripts/process-form-submission/main.ts
+++ google-scripts/process-form-submission/main.ts
@@ -1,3 +1,3 @@
 export function onFormSubmit(event: GoogleAppsScript.Events.FormsOnFormSubmit) {
-  Logger.log("Hello, world! (from circleci)", { event });
+  Logger.log("Hello, world! (from circleci) [test only push on main]", { event });
 }
