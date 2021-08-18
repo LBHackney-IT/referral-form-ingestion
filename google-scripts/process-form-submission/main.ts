9b9d0f2035a8c29767b4f0a733a3a643cef12b6a
JohnFarrellDev <johnfarrell@fastmail.com>
2021-08-18 20:10:18 +0100

    update main.ts to test push is 100% working

--- google-scripts/process-form-submission/main.ts
+++ google-scripts/process-form-submission/main.ts
@@ -1,3 +1,3 @@
 export function onFormSubmit(event: GoogleAppsScript.Events.FormsOnFormSubmit) {
-  Logger.log("Hello, world!", { event });
+  Logger.log("Hello, world! (from circleci)", { event });
 }
