7a3479ac0b4f074d8dc5e3660a0c6a2a775b9499
Miles-Alford <miles.alford@hackney.gov.uk>
2021-10-12 14:37:41 +0100

    WIP: add tests for createDocumentFromTemplate function within lib file

--- referral-form-data-process/google/lib.test.ts
+++ referral-form-data-process/google/lib.test.ts
@@ -1,17 +1,43 @@
+import { OAuth2Client } from "google-auth-library";
 import { createDocumentFromTemplate } from "./lib";
+import { drive_v3, google } from "googleapis";
+
+jest.mock("googleapis", () => ({ files: { copy: jest.fn() } }))
 
 describe("#createDocumentFromTemplate", () => {
+    const mockAuth = {
+        credentials: {
+            access_token: "actoken",
+            refresh_token: "rtoken",
+            scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+            token_type: "Bearer",
+            expiry_date: "1633950562052"
+        }
+    } as unknown as OAuth2Client
 
     it("should call google drive", () => {
+        const mockGoogleApiDrive = google.drive as jest.Mock
 
+        createDocumentFromTemplate(mockAuth, "test", "title", { one: "one" })
 
+        expect(mockGoogleApiDrive).toBeCalledWith({ version: "v3", auth: mockAuth })
     })
 
-    it("should call google sheet", () => {
+    it("should call google docs", () => {
+        const mockGoogleApiSheet = google.docs as jest.Mock
 
-        
-    })    
-    
-    
-    }
-) 
+        createDocumentFromTemplate(mockAuth, "test", "title", { one: "one" })
+
+        expect(mockGoogleApiSheet).toBeCalledWith({ version: "v1", auth: mockAuth })
+    })
+
+    it("should call google drive's copy function with the correct parameters", () => {
+        const mockGoogleApiDriveCopy = google.drive as unknown as jest.Mock<any>
+        createDocumentFromTemplate(mockAuth, "test", "title", { one: "one" })
+
+        expect(mockGoogleApiDriveCopy.files.copy).toBeCalledWith({})
+    })
+
+
+}
+)
