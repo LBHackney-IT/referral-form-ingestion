2187db5e49c655eb4e62d9b99a824f1421840c44
Miles-Alford <miles.alford@hackney.gov.uk>
2021-10-21 15:51:41 +0100

    WIP: update lambda function to generate google doc

--- referral-form-data-process/handler.ts
+++ referral-form-data-process/handler.ts
@@ -1,3 +1,22 @@
 import { SQSEvent } from "aws-lambda";
+import { createDocumentFromTemplate } from "./lib/createGoogleDocFromTemplate";
+import { generateAuth } from "./lib/generateGoogleAuth";
+import { getDataFromS3 } from "./lib/getDataFromS3";
 
-export const main = async (sqsEvent: SQSEvent) => {};
+export const main = async (sqsEvent: SQSEvent) => {
+  const auth = await generateAuth(
+    process.env.CLIENTEMAIL as string,
+    process.env.PRIVATEKEY as string
+  );
+  console.log("env is: " + process.env.CLIENTEMAIL);
+  const s3Data = await getDataFromS3(sqsEvent);
+  console.log("S3 response is: " + JSON.stringify(s3Data, null, 2));
+  const response = await createDocumentFromTemplate(
+    auth,
+    "1btL-4GSst9OxFxKCAHueX_kOr1M53YmwbvgV8JxsqIo",
+    "test",
+    s3Data as any
+  );
+
+  return response;
+};
