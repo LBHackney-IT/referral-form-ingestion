6699c04afce768c105b8b6e7bc70d6d5cc74d46e
JohnFarrellDev <johnfarrell@fastmail.com>
2021-10-21 09:40:08 +0100

    test getSecret functionality

--- referral-form-data-process/aws/getSecret.ts
+++ referral-form-data-process/aws/getSecret.ts
@@ -1,19 +1,23 @@
 import AWS from 'aws-sdk';
 
-const region = "eu-west-2";
-
-const secretsClient = new AWS.SecretsManager({
-    region
-});
-
 export const getSecret = (secretName: string) => {
+
+    const region = "eu-west-2";
+
+    const secretsClient = new AWS.SecretsManager({
+        region
+    });
+
+    let secretValue = '';
+
     secretsClient.getSecretValue({SecretId: secretName}, (_, data) => {
         if('SecretString' in data) {
-            return data.SecretString;
-        }
-        if(data.SecretBinary) {
+            secretValue = data.SecretString || '';
+        }else if(data.SecretBinary) {
             const buffer = Buffer.from(data.SecretBinary as WithImplicitCoercion<string>, 'base64');
-            return buffer.toString('ascii');
+            secretValue = buffer.toString('ascii');
         }
     })
+
+    return secretValue;
 }
