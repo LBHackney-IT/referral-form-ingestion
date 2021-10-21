72d485d09b3efa496e58e956c8b84f2f8d477f23
JohnFarrellDev <johnfarrell@fastmail.com>
2021-10-21 09:53:29 +0100

    formatting

--- referral-form-data-process/aws/getSecret.test.ts
+++ referral-form-data-process/aws/getSecret.test.ts
@@ -4,36 +4,37 @@ import { getSecret } from "./getSecret";
 
 const mockGetSecretValue = jest.fn();
 
-jest.mock('aws-sdk', () => ({
-    SecretsManager: jest.fn().mockImplementation(() => ({
-        getSecretValue: mockGetSecretValue
-    }))
+jest.mock("aws-sdk", () => ({
+  SecretsManager: jest.fn().mockImplementation(() => ({
+    getSecretValue: mockGetSecretValue,
+  })),
 }));
 
+describe("#getSecret", () => {
+  const testSecretName = "test-secret-name";
 
-describe('#getSecret', () => {
+  beforeEach(() => {
+    mockGetSecretValue.mockReset();
+  });
 
-    const testSecretName = "test-secret-name";
+  it("should make a secret client for eu-west-2", () => {
+    getSecret(testSecretName);
 
-    beforeEach(() => {
-        mockGetSecretValue.mockReset();
-    })
+    expect(AWS.SecretsManager).toBeCalledWith({ region: "eu-west-2" });
+  });
 
-    it('should make a secret client for eu-west-2', () => {
-        getSecret(testSecretName);
+  it("should make a request to get the value of a secret for a given name", () => {
+    getSecret(testSecretName);
 
-        expect(AWS.SecretsManager).toBeCalledWith({"region": "eu-west-2"});
-    })
+    expect(mockGetSecretValue).toBeCalledWith(
+      { SecretId: testSecretName },
+      expect.any(Function)
+    );
+  });
 
-    it('should make a request to get the value of a secret for a given name', () => {
-        getSecret(testSecretName);
+  it("should return an empty string if no secret value is retrieved", () => {
+    const response = getSecret(testSecretName);
 
-        expect(mockGetSecretValue).toBeCalledWith({"SecretId": testSecretName}, expect.any(Function));
-    })
-
-    it('should return an empty string if no secret value is retrieved', () => {
-        const response = getSecret(testSecretName);
-
-        expect(response).toBe("");
-    })
-})
+    expect(response).toBe("");
+  });
+});
--- referral-form-data-process/aws/getSecret.ts
+++ referral-form-data-process/aws/getSecret.ts
@@ -1,23 +1,25 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 
 export const getSecret = (secretName: string) => {
+  const region = "eu-west-2";
 
-    const region = "eu-west-2";
+  const secretsClient = new AWS.SecretsManager({
+    region,
+  });
 
-    const secretsClient = new AWS.SecretsManager({
-        region
-    });
+  let secretValue = "";
 
-    let secretValue = '';
+  secretsClient.getSecretValue({ SecretId: secretName }, (_, data) => {
+    if ("SecretString" in data) {
+      secretValue = data.SecretString || "";
+    } else if (data.SecretBinary) {
+      const buffer = Buffer.from(
+        data.SecretBinary as WithImplicitCoercion<string>,
+        "base64"
+      );
+      secretValue = buffer.toString("ascii");
+    }
+  });
 
-    secretsClient.getSecretValue({SecretId: secretName}, (_, data) => {
-        if('SecretString' in data) {
-            secretValue = data.SecretString || '';
-        }else if(data.SecretBinary) {
-            const buffer = Buffer.from(data.SecretBinary as WithImplicitCoercion<string>, 'base64');
-            secretValue = buffer.toString('ascii');
-        }
-    })
-
-    return secretValue;
-}
+  return secretValue;
+};
