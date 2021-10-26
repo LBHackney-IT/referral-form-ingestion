2c56684da92ca3767f6f414b8a4fcad38204e583
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2021-10-26 12:17:52 +0100

    Test token is generated when lambda is triggered

--- referral-form-data-process/main.test.ts
+++ referral-form-data-process/main.test.ts
@@ -1,8 +1,10 @@
 import { SQSEvent } from "aws-lambda/trigger/sqs";
+import { generateAuth } from "./lib/generateGoogleAuth";
 import { getDataFromS3 } from "./lib/getDataFromS3";
 import { handler } from "./main";
 
 jest.mock("./lib/getDataFromS3");
+jest.mock("./lib/generateGoogleAuth");
 
 describe("#handler", () => {
   const sqsTriggerEvent = {
@@ -22,11 +24,24 @@ describe("#handler", () => {
     (getDataFromS3 as jest.Mock).mockImplementation(() => {
       return s3ObjectArray;
     });
+
+    (generateAuth as jest.Mock).mockImplementation(() => {
+      return {};
+    });
   });
 
   it("it should call #getDataFromS3 when it receives an SQS event", async () => {
     await handler(sqsTriggerEvent);
 
     expect(getDataFromS3).toHaveBeenCalledWith(sqsTriggerEvent);
+  });
+
+  it("it should call #generateAuth when it receives an SQS event", async () => {
+    process.env.CLIENTEMAIL = "test-email";
+    process.env.PRIVATEKEY = "1234";
+
+    await handler(sqsTriggerEvent);
+
+    expect(generateAuth).toHaveBeenCalledWith("test-email", "1234");
   });
 });
--- referral-form-data-process/main.ts
+++ referral-form-data-process/main.ts
@@ -1,6 +1,12 @@
 import { SQSEvent } from "aws-lambda";
+import { generateAuth } from "./lib/generateGoogleAuth";
 import { getDataFromS3 } from "./lib/getDataFromS3";
 
 export const handler = async (sqsEvent: SQSEvent) => {
+  const clientEmail = process.env.CLIENTEMAIL as string;
+  const privateKey = process.env.PRIVATEKEY as string;
+
   const formDataObjects = await getDataFromS3(sqsEvent);
+
+  const googleAuthToken = await generateAuth(clientEmail, privateKey);
 };
