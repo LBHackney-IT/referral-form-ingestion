099a8cf60bb66fce82f4225c8c778fbef7e7e2ae
Renny Omorinsola Fadoju <32823756+Renny4Real@users.noreply.github.com>
2021-11-15 11:56:35 +0000

    Fix(SCT-1365): Add fixes from testing lambda in test environment (#35)
    * Add fixes from testing lambda in test environment
    
    - Format private key
    This formats the private key of the service account. The reason for this is because when adding the key to the environment variables it does not format the new line characters and the auth fails.
    
    - Add `Promise.all`
    This encapsulates the code that loops through each form data object within a Promise.all. This means that the code will wait for all the await functions to resolve before exiting the lambda. Without these, the lambda doesn't wait for the entire doc generation to finish and exits prematurely.
    
    - Add timeout of 30 seconds to the lambda.
    The default timeout is 6 seconds. This updates it so that the lambda is up for 30 seconds before terminating itself.
    
    Co-authored-by: Miles-Alford <miles.alford@hackney.gov.uk>
    
    * Make environment variables more readable
    
    * Remove 30 second timeout
    
    * Update the CI config to match Env variable case
    
    * Refactor promise statement for doc generation
    
    Co-authored-by: Miles-Alford <miles.alford@hackney.gov.uk>

--- referral-form-data-process/main.ts
+++ referral-form-data-process/main.ts
@@ -4,22 +4,23 @@ import { generateAuth } from "./lib/generateGoogleAuth";
 import { getDataFromS3 } from "./lib/getDataFromS3";
 
 export const handler = async (sqsEvent: SQSEvent) => {
-  const clientEmail = process.env.CLIENTEMAIL as string;
-  const privateKey = process.env.PRIVATEKEY as string;
-  const templateDocumentId = process.env.TEMPLATEDOCUMENTID as string;
+  const clientEmail = process.env.CLIENT_EMAIL as string;
+  const privateKey = process.env.PRIVATE_KEY as string;
+  const templateDocumentId = process.env.TEMPLATE_DOCUMENT_ID as string;
   const title = process.env.TITLE as string;
+  const formattedPrivateKey = privateKey.replace(/\\n/g, "\n");
 
   const formDataObjects = await getDataFromS3(sqsEvent);
+  const googleAuthToken = await generateAuth(clientEmail, formattedPrivateKey);
 
-  const googleAuthToken = await generateAuth(clientEmail, privateKey);
-
-  formDataObjects.forEach(
-    async (formData) =>
-      await createDocumentFromTemplate(
+  await Promise.all(
+    formDataObjects.map((formData) =>
+      createDocumentFromTemplate(
         googleAuthToken,
         templateDocumentId,
         title,
         formData
       )
+    )
   );
 };
