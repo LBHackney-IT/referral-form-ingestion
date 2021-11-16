f3f1ee2f887291985ebaa48f0b2def46a115c29d
Miles-Alford <miles.alford@hackney.gov.uk>
2021-11-16 11:59:25 +0000

    Allow lambda to add document url to spreadsheet

--- referral-form-data-process/main.ts
+++ referral-form-data-process/main.ts
@@ -1,4 +1,5 @@
 import { SQSEvent } from "aws-lambda";
+import { addGoogleDocUrlToSheet } from "./lib/addGoogleDocUrlToSheet";
 import { createDocumentFromTemplate } from "./lib/createGoogleDocFromTemplate";
 import { generateAuth } from "./lib/generateGoogleAuth";
 import { getDataFromS3 } from "./lib/getDataFromS3";
@@ -8,19 +9,27 @@ export const handler = async (sqsEvent: SQSEvent) => {
   const privateKey = process.env.PRIVATE_KEY as string;
   const templateDocumentId = process.env.TEMPLATE_DOCUMENT_ID as string;
   const title = process.env.TITLE as string;
+  const urlColumn = process.env.URL_COLUMN as string;
   const formattedPrivateKey = privateKey.replace(/\\n/g, "\n");
 
   const formDataObjects = await getDataFromS3(sqsEvent);
   const googleAuthToken = await generateAuth(clientEmail, formattedPrivateKey);
 
   await Promise.all(
-    formDataObjects.map((formData) =>
-      createDocumentFromTemplate(
+    formDataObjects.map(async (formData) => {
+      const createdDocument = await createDocumentFromTemplate(
         googleAuthToken,
         templateDocumentId,
         title,
         formData
-      )
-    )
+      );
+      const documentUrl = `https://docs.google.com/spreadsheets/d/${createdDocument.documentId}/edit`;
+      await addGoogleDocUrlToSheet(
+        googleAuthToken,
+        documentUrl,
+        urlColumn,
+        formData.FormRow.toString()
+      );
+    })
   );
 };
