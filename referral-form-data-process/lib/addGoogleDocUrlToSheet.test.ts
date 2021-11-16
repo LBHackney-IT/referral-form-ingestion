07f14ec8311d34aa7d470e519af61a97ee2a3945
Miles-Alford <miles.alford@hackney.gov.uk>
2021-11-16 11:02:44 +0000

    Lint

--- referral-form-data-process/lib/addGoogleDocUrlToSheet.test.ts
+++ referral-form-data-process/lib/addGoogleDocUrlToSheet.test.ts
@@ -8,163 +8,163 @@ const mockUpdate = jest.fn();
 jest.mock("googleapis");
 
 describe.only("#addGoogleDocUrlToSheet", () => {
-    const mockAuth = {
-        credentials: {
-            access_token: "actoken",
-            refresh_token: "rtoken",
-            scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
-            token_type: "Bearer",
-            expiry_date: "1633950562052",
+  const mockAuth = {
+    credentials: {
+      access_token: "actoken",
+      refresh_token: "rtoken",
+      scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+      token_type: "Bearer",
+      expiry_date: "1633950562052",
+    },
+  } as unknown as OAuth2Client;
+
+  const testDocumentUrl = "test-document-url";
+  const testColumnUrl = "test-column-url";
+  const testTableRow = "test-table-row";
+  const testSpreadsheetId = "test-spreadsheet-id";
+  const googleSheetVersion = "v4";
+
+  beforeEach(() => {
+    (google.sheets as jest.Mock).mockImplementation(() => ({
+      spreadsheets: {
+        get: mockGet,
+        values: {
+          update: mockUpdate,
         },
-    } as unknown as OAuth2Client;
+      },
+    }));
 
-    const testDocumentUrl = "test-document-url";
-    const testColumnUrl = "test-column-url";
-    const testTableRow = "test-table-row";
-    const testSpreadsheetId = "test-spreadsheet-id";
-    const googleSheetVersion = "v4";
+    mockGet.mockImplementation(() => ({
+      data: {
+        spreadsheetId: testSpreadsheetId,
+      },
+    }));
+  });
 
-    beforeEach(() => {
-        (google.sheets as jest.Mock).mockImplementation(() => ({
-            spreadsheets: {
-                get: mockGet,
-                values: {
-                    update: mockUpdate,
-                },
-            },
-        }));
+  it("should call to sheets with our auth token", async () => {
+    await addGoogleDocUrlToSheet(
+      mockAuth,
+      testDocumentUrl,
+      testColumnUrl,
+      testTableRow
+    );
 
-        mockGet.mockImplementation(() => ({
-            data: {
-                spreadsheetId: testSpreadsheetId,
-            },
-        }));
+    expect(google.sheets).toBeCalledWith({
+      version: googleSheetVersion,
+      auth: mockAuth,
     });
+  });
 
-    it("should call to sheets with our auth token", async () => {
-        await addGoogleDocUrlToSheet(
-            mockAuth,
-            testDocumentUrl,
-            testColumnUrl,
-            testTableRow
-        );
+  it("should throw an error if sheet is undefined", async () => {
+    mockGet.mockImplementation(() => ({
+      data: undefined,
+    }));
 
-        expect(google.sheets).toBeCalledWith({
-            version: googleSheetVersion,
-            auth: mockAuth,
-        });
+    try {
+      await addGoogleDocUrlToSheet(
+        mockAuth,
+        testDocumentUrl,
+        testColumnUrl,
+        testTableRow
+      );
+      fail("addGoogleDocUrlToSheet should have thrown an exception");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        expect(e.message).toBe("Unable to fetch sheet based on provided ID");
+      } else {
+        fail("exception should be instance of Error");
+      }
+    }
+  });
+
+  it("should throw an error if spreadsheetId is an empty string", async () => {
+    mockGet.mockImplementation(() => ({
+      data: {
+        spreadsheetId: "",
+      },
+    }));
+
+    try {
+      await addGoogleDocUrlToSheet(
+        mockAuth,
+        testDocumentUrl,
+        testColumnUrl,
+        testTableRow
+      );
+      fail("addGoogleDocUrlToSheet should have thrown an exception");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        expect(e.message).toBe("Unable to fetch sheet based on provided ID");
+      } else {
+        fail("exception should be instance of Error");
+      }
+    }
+  });
+
+  it("should throw an error if spreadsheetId is undefined", async () => {
+    mockGet.mockImplementation(() => ({
+      data: {
+        spreadsheetId: undefined,
+      },
+    }));
+
+    try {
+      await addGoogleDocUrlToSheet(
+        mockAuth,
+        testDocumentUrl,
+        testColumnUrl,
+        testTableRow
+      );
+      fail("addGoogleDocUrlToSheet should have thrown an exception");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        expect(e.message).toBe("Unable to fetch sheet based on provided ID");
+      } else {
+        fail("exception should be instance of Error");
+      }
+    }
+  });
+
+  it("should throw an error if spreadsheetId is null", async () => {
+    mockGet.mockImplementation(() => ({
+      data: {
+        spreadsheetId: null,
+      },
+    }));
+
+    try {
+      await addGoogleDocUrlToSheet(
+        mockAuth,
+        testDocumentUrl,
+        testColumnUrl,
+        testTableRow
+      );
+      fail("addGoogleDocUrlToSheet should have thrown an exception");
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        expect(e.message).toBe("Unable to fetch sheet based on provided ID");
+      } else {
+        fail("exception should be instance of Error");
+      }
+    }
+  });
+
+  it("should call to update the spreadsheet values", async () => {
+    await addGoogleDocUrlToSheet(
+      mockAuth,
+      testDocumentUrl,
+      testColumnUrl,
+      testTableRow
+    );
+
+    expect(mockUpdate).toBeCalledWith({
+      range: `${testColumnUrl}${testTableRow}`,
+      requestBody: {
+        range: `${testColumnUrl}${testTableRow}`,
+        values: [["test-document-url"]],
+      },
+      spreadsheetId: testSpreadsheetId,
+      valueInputOption: "USER_ENTERED",
     });
-
-    it("should throw an error if sheet is undefined", async () => {
-        mockGet.mockImplementation(() => ({
-            data: undefined,
-        }));
-
-        try {
-            await addGoogleDocUrlToSheet(
-                mockAuth,
-                testDocumentUrl,
-                testColumnUrl,
-                testTableRow
-            );
-            fail("addGoogleDocUrlToSheet should have thrown an exception");
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                expect(e.message).toBe("Unable to fetch sheet based on provided ID");
-            } else {
-                fail("exception should be instance of Error");
-            }
-        }
-    });
-
-    it("should throw an error if spreadsheetId is an empty string", async () => {
-        mockGet.mockImplementation(() => ({
-            data: {
-                spreadsheetId: "",
-            },
-        }));
-
-        try {
-            await addGoogleDocUrlToSheet(
-                mockAuth,
-                testDocumentUrl,
-                testColumnUrl,
-                testTableRow
-            );
-            fail("addGoogleDocUrlToSheet should have thrown an exception");
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                expect(e.message).toBe("Unable to fetch sheet based on provided ID");
-            } else {
-                fail("exception should be instance of Error");
-            }
-        }
-    });
-
-    it("should throw an error if spreadsheetId is undefined", async () => {
-        mockGet.mockImplementation(() => ({
-            data: {
-                spreadsheetId: undefined,
-            },
-        }));
-
-        try {
-            await addGoogleDocUrlToSheet(
-                mockAuth,
-                testDocumentUrl,
-                testColumnUrl,
-                testTableRow
-            );
-            fail("addGoogleDocUrlToSheet should have thrown an exception");
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                expect(e.message).toBe("Unable to fetch sheet based on provided ID");
-            } else {
-                fail("exception should be instance of Error");
-            }
-        }
-    });
-
-    it("should throw an error if spreadsheetId is null", async () => {
-        mockGet.mockImplementation(() => ({
-            data: {
-                spreadsheetId: null,
-            },
-        }));
-
-        try {
-            await addGoogleDocUrlToSheet(
-                mockAuth,
-                testDocumentUrl,
-                testColumnUrl,
-                testTableRow
-            );
-            fail("addGoogleDocUrlToSheet should have thrown an exception");
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                expect(e.message).toBe("Unable to fetch sheet based on provided ID");
-            } else {
-                fail("exception should be instance of Error");
-            }
-        }
-    });
-
-    it("should call to update the spreadsheet values", async () => {
-        await addGoogleDocUrlToSheet(
-            mockAuth,
-            testDocumentUrl,
-            testColumnUrl,
-            testTableRow
-        );
-
-        expect(mockUpdate).toBeCalledWith({
-            range: `${testColumnUrl}${testTableRow}`,
-            requestBody: {
-                range: `${testColumnUrl}${testTableRow}`,
-                values: [["test-document-url"]],
-            },
-            spreadsheetId: testSpreadsheetId,
-            valueInputOption: "USER_ENTERED",
-        });
-    });
+  });
 });
--- referral-form-data-process/lib/addGoogleDocUrlToSheet.ts
+++ referral-form-data-process/lib/addGoogleDocUrlToSheet.ts
@@ -2,33 +2,33 @@ import { OAuth2Client } from "google-auth-library";
 import { google } from "googleapis";
 
 export const addGoogleDocUrlToSheet = async (
-    auth: OAuth2Client,
-    documentURL: string,
-    urlColumn: string,
-    tableRow: string
+  auth: OAuth2Client,
+  documentURL: string,
+  urlColumn: string,
+  tableRow: string
 ) => {
-    const sheets = google.sheets({
-        version: "v4",
-        auth,
-    });
+  const sheets = google.sheets({
+    version: "v4",
+    auth,
+  });
 
-    const spreadsheetId = process.env.SPREADSHEET_ID as string;
+  const spreadsheetId = process.env.SPREADSHEET_ID as string;
 
-    const { data: sheet } = await sheets.spreadsheets.get({
-        spreadsheetId,
-    });
+  const { data: sheet } = await sheets.spreadsheets.get({
+    spreadsheetId,
+  });
 
-    if (!sheet || !sheet.spreadsheetId) {
-        throw new Error("Unable to fetch sheet based on provided ID");
-    }
+  if (!sheet || !sheet.spreadsheetId) {
+    throw new Error("Unable to fetch sheet based on provided ID");
+  }
 
-    await sheets.spreadsheets.values.update({
-        spreadsheetId: sheet.spreadsheetId,
-        range: `${urlColumn}${tableRow}`,
-        valueInputOption: "USER_ENTERED",
-        requestBody: {
-            range: `${urlColumn}${tableRow}`,
-            values: [[documentURL]],
-        },
-    });
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: sheet.spreadsheetId,
+    range: `${urlColumn}${tableRow}`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: {
+      range: `${urlColumn}${tableRow}`,
+      values: [[documentURL]],
+    },
+  });
 };
