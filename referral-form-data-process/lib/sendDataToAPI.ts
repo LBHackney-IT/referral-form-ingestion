40a2469119931be96e7462034a9ea41047f5e565
Miles-Alford <miles.alford@hackney.gov.uk>
2021-11-19 09:48:53 +0000

    Update api call function to send object rather than array

--- referral-form-data-process/lib/sendDataToAPI.ts
+++ referral-form-data-process/lib/sendDataToAPI.ts
@@ -1,46 +1,37 @@
 import axios from "axios";
-interface PostRequest {
-    referrer: string;
-    requestedSupport: string;
-    clients: string[];
-    referralUri: string;
-}
 
 export const sendDataToAPI = async (
-    formData: Record<string, string[]>,
-    documentURL: string
+  formData: Record<string, string[]>,
+  documentURL: string
 ) => {
-    const ENDPOINT_API = process.env.ENDPOINT_API as string;
-    const AWS_KEY = process.env.AWS_KEY as string;
-    const headers = { "x-api-key": AWS_KEY };
+  const ENDPOINT_API = process.env.ENDPOINT_API as string;
+  const AWS_KEY = process.env.AWS_KEY as string;
+  const headers = { "x-api-key": AWS_KEY };
 
-    const batchPostRequests: PostRequest[] = [];
-    let referrerValue: string = "";
-    let requestedSupportValue: string = "";
-    let clientsValue: string[] = [];
+  let referrerValue: string | undefined;
+  let requestedSupportValue: string | undefined;
+  let clientsValue: string[] | undefined;
 
-    const dataEntries = Object.entries(formData);
-    for (const [key, value] of dataEntries) {
-        switch (key) {
-            case "TEMP: Question for getting the referrer of the form":
-                referrerValue = value.toString();
-                break;
-            case "TEMP: Question for getting the requested support":
-                requestedSupportValue = value.toString();
-                break;
-            case "TEMP: Question for getting the people being referred":
-                clientsValue = value;
-                break;
-        }
+  const dataEntries = Object.entries(formData);
+  for (const [key, value] of dataEntries) {
+    switch (key) {
+      case "TEMP: Question for getting the referrer of the form":
+        referrerValue = value.toString();
+        break;
+      case "TEMP: Question for getting the requested support":
+        requestedSupportValue = value.toString();
+        break;
+      case "TEMP: Question for getting the people being referred":
+        clientsValue = value;
+        break;
     }
-    const postObject = {
-        referrer: referrerValue,
-        requestedSupport: requestedSupportValue,
-        clients: clientsValue,
-        referralUri: documentURL,
-    };
+  }
+  const postObject = {
+    referrer: referrerValue,
+    requestedSupport: requestedSupportValue,
+    clients: clientsValue,
+    referralUri: documentURL,
+  };
 
-    batchPostRequests.push(postObject);
-
-    await axios.post(`${ENDPOINT_API}/`, batchPostRequests, { headers: headers });
+  await axios.post(`${ENDPOINT_API}/`, postObject, { headers: headers });
 };
