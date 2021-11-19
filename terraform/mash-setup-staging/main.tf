ac17679a1b05031645bdc850be4baf63ae4f46e1
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2021-11-19 12:21:47 +0000

    Add secretsmanager resources to store google secrets

--- terraform/mash-setup-staging/main.tf
+++ terraform/mash-setup-staging/main.tf
@@ -18,3 +18,23 @@ module "mash_data_processing" {
   region      = "eu-west-2"
   environment = "stg"
 }
+
+resource "aws_secretsmanager_secret" "gcp_service_account_client_email" {
+  name        = "${local.resource_prefix}-gcp-service-account-client-email"
+  description = "The client email associated with the Social Care Referrals Google Service Account"
+}
+
+resource "aws_secretsmanager_secret" "gcp_service_account_private_key" {
+  name        = "${local.resource_prefix}-gcp-service-account-private-key"
+  description = "The private key for the Social Care Referrals Google Service Account"
+}
+
+resource "aws_secretsmanager_secret" "referrals_google_doc_template_id" {
+  name        = "${local.resource_prefix}-referrals-google-doc-template-id"
+  description = "The ID of the MASH Google template document"
+}
+
+resource "aws_secretsmanager_secret" "referrals_google_spreadsheet_id" {
+  name        = "${local.resource_prefix}-referrals-google-spreadsheet-id"
+  description = "The ID of the MASH spreadsheet"
+}
