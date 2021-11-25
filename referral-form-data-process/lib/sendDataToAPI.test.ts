9237749c93a65d5e7f6d3e3f783be1ecab4508b7
Miles-Alford <miles.alford@hackney.gov.uk>
2021-11-25 11:06:15 +0000

    Refactor SendDataToAPI function

--- referral-form-data-process/lib/sendDataToAPI.test.ts
+++ referral-form-data-process/lib/sendDataToAPI.test.ts
@@ -4,19 +4,24 @@ jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("#sendDataToAPI", () => {
-  const testReferrerQuestion =
-    "TEMP: Question for getting the referrer of the form";
+  const testReferrerFirstNameQuestion = "Referrer First Name";
+  const testReferrerLastNameQuestion = "Referrer Last Name";
   const testSupportQuestion =
-    "TEMP: Question for getting the requested support";
-  const testClientsQuestion =
-    "TEMP: Question for getting the people being referred";
-  const testReferrerAnswer = "test name";
+    "What support do you think the child(ren) would benefit from?";
+  const testClientOneFirstNameQuestion = "Child 1: Child's First Name";
+  const testClientOneLastNameQuestion = "Child 1: Child's Last Name";
+  const testReferrerFirstNameAnswer = "First";
+  const testReferrerLastNameAnswer = "Referrer";
   const testSupportAnswer = "test support";
-  const testClientsAnswer = "test client";
+  const testClientOneFirstNameAnswer = "Client";
+  const testClientOneLastNameAnswer = "One";
+  const testClientOneCompleteName = "Client One";
   const testFormData = {
-    [testReferrerQuestion]: [testReferrerAnswer],
+    [testReferrerFirstNameQuestion]: [testReferrerFirstNameAnswer],
+    [testReferrerLastNameQuestion]: [testReferrerLastNameAnswer],
     [testSupportQuestion]: [testSupportAnswer],
-    [testClientsQuestion]: [testClientsAnswer],
+    [testClientOneFirstNameQuestion]: [testClientOneFirstNameAnswer],
+    [testClientOneLastNameQuestion]: [testClientOneLastNameAnswer],
   };
   const testDocumentUrl = "test-document-url";
   const testEndpoint = "test-endpoint-url";
@@ -31,9 +36,45 @@ describe("#sendDataToAPI", () => {
   it("should call the service API with the correct parameters", async () => {
     mockedAxios.post.mockResolvedValue({ data: {} });
     const mockData = {
-      referrer: testReferrerAnswer,
+      referrer: `${testReferrerFirstNameAnswer} ${testReferrerLastNameAnswer}`,
       requestedSupport: testSupportAnswer,
-      clients: [testClientsAnswer],
+      clients: [testClientOneCompleteName],
+      referralUri: testDocumentUrl,
+    };
+
+    await sendDataToAPI(testFormData, testDocumentUrl);
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      `${testEndpoint}/mash-referral`,
+      mockData,
+      {
+        headers: {
+          "x-api-key": testAPIKey,
+        },
+      }
+    );
+  });
+
+  it("should send the correct number of clients if multiple clients are inputted", async () => {
+    const testClientTwoFirstNameQuestion = "Child 2: Child's First Name";
+    const testClientTwoLastNameQuestion = "Child 2: Child's Last Name";
+    const testClientTwoFirstNameAnswer = "Client";
+    const testClientTwoLastNameAnswer = "Two";
+    const testClientTwoCompleteName = "Client Two";
+    const testFormData = {
+      [testReferrerFirstNameQuestion]: [testReferrerFirstNameAnswer],
+      [testReferrerLastNameQuestion]: [testReferrerLastNameAnswer],
+      [testSupportQuestion]: [testSupportAnswer],
+      [testClientOneFirstNameQuestion]: [testClientOneFirstNameAnswer],
+      [testClientOneLastNameQuestion]: [testClientOneLastNameAnswer],
+      [testClientTwoFirstNameQuestion]: [testClientTwoFirstNameAnswer],
+      [testClientTwoLastNameQuestion]: [testClientTwoLastNameAnswer],
+    };
+    mockedAxios.post.mockResolvedValue({ data: {} });
+    const mockData = {
+      referrer: `${testReferrerFirstNameAnswer} ${testReferrerLastNameAnswer}`,
+      requestedSupport: testSupportAnswer,
+      clients: [testClientOneCompleteName, testClientTwoCompleteName],
       referralUri: testDocumentUrl,
     };
 
--- referral-form-data-process/lib/sendDataToAPI.ts
+++ referral-form-data-process/lib/sendDataToAPI.ts
@@ -1,5 +1,27 @@
 import axios from "axios";
 
+interface FormDataAnswersObject {
+  referrerFirstName: string | undefined;
+  referrerLastName: string | undefined;
+  requestedSupport: string | undefined;
+  clientOneFirstName: string | undefined;
+  clientOneLastName: string | undefined;
+  clientTwoFirstName: string | undefined;
+  clientTwoLastName: string | undefined;
+  clientThreeFirstName: string | undefined;
+  clientThreeLastName: string | undefined;
+  clientFourFirstName: string | undefined;
+  clientFourLastName: string | undefined;
+  clientFiveFirstName: string | undefined;
+  clientFiveLastName: string | undefined;
+  clientSixFirstName: string | undefined;
+  clientSixLastName: string | undefined;
+  clientSevenFirstName: string | undefined;
+  clientSevenLastName: string | undefined;
+  clientEightFirstName: string | undefined;
+  clientEightLastName: string | undefined;
+}
+
 export const sendDataToAPI = async (
   formData: Record<string, string[]>,
   documentURL: string
@@ -7,28 +29,153 @@ export const sendDataToAPI = async (
   const ENDPOINT_API = process.env.ENDPOINT_API as string;
   const AWS_KEY = process.env.AWS_KEY as string;
   const headers = { "x-api-key": AWS_KEY };
-
-  let referrerValue: string | undefined;
-  let requestedSupportValue: string | undefined;
-  let clientsValue: string[] | undefined;
+  let formDataAnswersObject: FormDataAnswersObject = {
+    referrerFirstName: undefined,
+    referrerLastName: undefined,
+    requestedSupport: undefined,
+    clientOneFirstName: undefined,
+    clientOneLastName: undefined,
+    clientTwoFirstName: undefined,
+    clientTwoLastName: undefined,
+    clientThreeFirstName: undefined,
+    clientThreeLastName: undefined,
+    clientFourFirstName: undefined,
+    clientFourLastName: undefined,
+    clientFiveFirstName: undefined,
+    clientFiveLastName: undefined,
+    clientSixFirstName: undefined,
+    clientSixLastName: undefined,
+    clientSevenFirstName: undefined,
+    clientSevenLastName: undefined,
+    clientEightFirstName: undefined,
+    clientEightLastName: undefined,
+  };
+  let clientsValue: string[] | undefined = [];
 
   const dataEntries = Object.entries(formData);
   for (const [key, value] of dataEntries) {
-    switch (key) {
-      case "TEMP: Question for getting the referrer of the form":
-        referrerValue = value.toString();
-        break;
-      case "TEMP: Question for getting the requested support":
-        requestedSupportValue = value.toString();
-        break;
-      case "TEMP: Question for getting the people being referred":
-        clientsValue = value;
-        break;
+    if (key === "Referrer First Name") {
+      formDataAnswersObject.referrerFirstName = value.toString();
+    }
+    if (key === "Referrer Last Name") {
+      formDataAnswersObject.referrerLastName = value.toString();
+    }
+    if (
+      key === "What support do you think the child(ren) would benefit from?"
+    ) {
+      formDataAnswersObject.requestedSupport = value.toString();
+    }
+    if (key === "Child 1: Child's First Name") {
+      formDataAnswersObject.clientOneFirstName = value.toString();
+    }
+    if (key === "Child 1: Child's Last Name") {
+      formDataAnswersObject.clientOneLastName = value.toString();
+    }
+    if (key === "Child 2: Child's First Name") {
+      formDataAnswersObject.clientTwoFirstName = value.toString();
+    }
+    if (key === "Child 2: Child's Last Name") {
+      formDataAnswersObject.clientTwoLastName = value.toString();
+    }
+    if (key === "Child 3: Child's First Name") {
+      formDataAnswersObject.clientThreeFirstName = value.toString();
+    }
+    if (key === "Child 3: Child's Last Name") {
+      formDataAnswersObject.clientThreeLastName = value.toString();
+    }
+    if (key === "Child 4: Child's First Name") {
+      formDataAnswersObject.clientFourFirstName = value.toString();
+    }
+    if (key === "Child 4: Child's Last Name") {
+      formDataAnswersObject.clientFourLastName = value.toString();
+    }
+    if (key === "Child 5: Child's First Name") {
+      formDataAnswersObject.clientFiveFirstName = value.toString();
+    }
+    if (key === "Child 5: Child's Last Name") {
+      formDataAnswersObject.clientFiveLastName = value.toString();
+    }
+    if (key === "Child 6: Child's First Name") {
+      formDataAnswersObject.clientSixFirstName = value.toString();
+    }
+    if (key === "Child 6: Child's Last Name") {
+      formDataAnswersObject.clientSixLastName = value.toString();
+    }
+    if (key === "Child 7: Child's First Name") {
+      formDataAnswersObject.clientSevenFirstName = value.toString();
+    }
+    if (key === "Child 7: Child's Last Name") {
+      formDataAnswersObject.clientSevenLastName = value.toString();
+    }
+    if (key === "Child 8: Child's First Name") {
+      formDataAnswersObject.clientEightFirstName = value.toString();
+    }
+    if (key === "Child 8: Child's Last Name") {
+      formDataAnswersObject.clientEightLastName = value.toString();
     }
   }
+
+  const clientOne = getClientFullName(
+    formDataAnswersObject.clientOneFirstName,
+    formDataAnswersObject.clientOneLastName
+  );
+  const clientTwo = getClientFullName(
+    formDataAnswersObject.clientTwoFirstName,
+    formDataAnswersObject.clientTwoLastName
+  );
+  const clientThree = getClientFullName(
+    formDataAnswersObject.clientThreeFirstName,
+    formDataAnswersObject.clientThreeLastName
+  );
+  const clientFour = getClientFullName(
+    formDataAnswersObject.clientFourFirstName,
+    formDataAnswersObject.clientFourLastName
+  );
+  const clientFive = getClientFullName(
+    formDataAnswersObject.clientFiveFirstName,
+    formDataAnswersObject.clientFiveLastName
+  );
+  const clientSix = getClientFullName(
+    formDataAnswersObject.clientSixFirstName,
+    formDataAnswersObject.clientSixLastName
+  );
+  const clientSeven = getClientFullName(
+    formDataAnswersObject.clientSevenFirstName,
+    formDataAnswersObject.clientSevenLastName
+  );
+  const clientEight = getClientFullName(
+    formDataAnswersObject.clientEightFirstName,
+    formDataAnswersObject.clientEightLastName
+  );
+
+  if (clientOne) {
+    clientsValue.push(clientOne);
+  }
+  if (clientTwo) {
+    clientsValue.push(clientTwo);
+  }
+  if (clientThree) {
+    clientsValue.push(clientThree);
+  }
+  if (clientFour) {
+    clientsValue.push(clientFour);
+  }
+  if (clientFive) {
+    clientsValue.push(clientFive);
+  }
+  if (clientSix) {
+    clientsValue.push(clientSix);
+  }
+  if (clientSeven) {
+    clientsValue.push(clientSeven);
+  }
+  if (clientEight) {
+    clientsValue.push(clientEight);
+  }
+
   const postObject = {
-    referrer: referrerValue,
-    requestedSupport: requestedSupportValue,
+    referrer: `${formDataAnswersObject.referrerFirstName} ${formDataAnswersObject.referrerLastName}`,
+    requestedSupport: formDataAnswersObject.requestedSupport,
     clients: clientsValue,
     referralUri: documentURL,
   };
@@ -37,3 +184,9 @@ export const sendDataToAPI = async (
     headers: headers,
   });
 };
+
+function getClientFullName(firstName?: string, lastName?: string) {
+  if (firstName && lastName) {
+    return `${firstName} ${lastName}`;
+  } else return null;
+}
