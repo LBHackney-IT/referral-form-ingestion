bd2ea13a7059ad975ae1690bfcf58ea51bc79bc6
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2021-12-06 16:15:57 +0000

    Move setup for required providers into the module

--- terraform/mash-setup-staging/main.tf
+++ terraform/mash-setup-staging/main.tf
@@ -1,16 +1,6 @@
 provider "aws" {
   region = "eu-west-2"
 }
-
-terraform {
-  backend "s3" {}
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 module "mash_data_processing" {
   source = "../modules/api-proxy-s3-sqs"
 
