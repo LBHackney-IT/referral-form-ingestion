e6b9325f837417290f53f51098c34402f5e602f6
Miles-Alford <miles.alford@hackney.gov.uk>
2021-12-13 10:48:12 +0000

    Remove uneeded code

--- google-scripts/process-form-submission/getProperties.ts
+++ google-scripts/process-form-submission/getProperties.ts
@@ -1,15 +1,5 @@
 export function getProperties() {
-  const { getProperty, setProperty } = PropertiesService.getScriptProperties();
-
-  /* The lines below are a skeleton to allow a developer to manually change the values of the properties required to let the script function correctly.
-     In the Apps Script project these lines will have to be uncommented and the values will have to be replaced with the correct ones.
-     This is because clasp will replace the entire script with the local code and the script is owned by the Drive it lives in which does not give us the ability to change the properties through the UI.
-  */
-
-  // setProperty("MASH_SHEET_NAME", "MASH_SHEET_VALUE")
-  // setProperty("REFERRALS_BUCKET_URL", "REFERRALS_BUCKET_URL_VALUE")
-  // setProperty("REFERRALS_BUCKET_API_KEY", "REFERRALS_BUCKET_API_KEY_VALUE")
-  // setProperty("UNIQUE_ID_COLUMN_NO", "UNIQUE_ID_COLUMN_NO_VALUE")
+  const { getProperty } = PropertiesService.getScriptProperties();
 
   const REFERRALS_SHEET_NAME = getProperty("MASH_SHEET_NAME");
   const S3_ENDPOINT_API = getProperty("REFERRALS_BUCKET_URL");
